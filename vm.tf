597af5d964e7555878b8d4a60ae8888527aed1e6
estelle-ngo <paulangono@gmail.com>
2025-11-06 00:45:26 +0100

    Create vm.tf

---
+++ vm.tf
@@ -0,0 +1,37 @@
+resource "azurerm_network_interface" "nic" {
+  count               = length(var.vm_names)
+  name                = "${var.vm_names[count.index]}-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  count               = length(var.vm_names)
+  name                = var.vm_names[count.index]
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [
+    azurerm_network_interface.nic[count.index].id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
